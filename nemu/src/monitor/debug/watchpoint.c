a18b22c3f68499ce239cd0a4721785c9fb285c92
tracer-ics2018 <tracer@njuics.org>
2018-12-11 22:35:51 -0500

    >  compile U201514566 root Linux localhost.localdomain 3.10.0-862.14.4.el7.x86_64 #1 SMP Wed Sep 26 15:12:11 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  22:35:51 up 28 days, 18:05,  2 users,  load average: 0.01, 0.03, 0.05 a506b7674f40d1fbfecfa3a973ebca7a2f7508b9

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -27,6 +27,7 @@ WP* new_wp(char* args){
 	free_ = wp_ptr->next;
 	wp_ptr->next = head;
 	wp_ptr->expr_str = args;
+	printf("%s\n", args);
 	wp_ptr->old_value = expr(args, &isSuccess);
 	assert(isSuccess);
 	return head = wp_ptr;
