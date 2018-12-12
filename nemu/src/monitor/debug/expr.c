d3745976cf573369a642bec708a47983fd1d3c7e
tracer-ics2018 <tracer@njuics.org>
2018-12-12 08:54:36 -0500

    >  compile U201514566 root Linux localhost.localdomain 3.10.0-862.14.4.el7.x86_64 #1 SMP Wed Sep 26 15:12:11 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  08:54:36 up 29 days,  4:24,  2 users,  load average: 0.00, 0.04, 0.05 92e662f76642768b222b3e6fa59cbed32e2ad8fd

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -206,6 +206,9 @@ int eval(int p, int q) {
       case '-': return val1 - val2;
       case '*': return val1 * val2;
       case '/': return val1 / val2;
+			case TK_NEQ: return val1 != val2;
+			case TK_EQ: return val1 == val2;
+			case TK_AND: return val1 && val2;
       default: assert(0);
     }	
   }
