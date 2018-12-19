3610a79737c1eceda2ffd562d1a49deca880b87e
tracer-ics2018 <tracer@njuics.org>
2018-12-18 21:01:29 -0500

    >  compile U201514566 root Linux localhost.localdomain 3.10.0-862.14.4.el7.x86_64 #1 SMP Wed Sep 26 15:12:11 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  21:01:29 up 35 days, 16:31,  3 users,  load average: 0.00, 0.03, 0.05 5ed979dfa70ec3fb727018252e3766a548778e25

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -87,6 +87,6 @@ make_EHelper(movzx) {
 
 make_EHelper(lea) {
   operand_write(id_dest, &id_src->addr);
-	printf("esp:%d, 0x%x", id_src->addr, id_src->addr);
+	printf("esp:%d, 0x%x\n", id_src->addr, id_src->addr);
   print_asm_template2(lea);
 }
