c13827c08eb1b8e1eef38adb314ffe1b8073e0cc
tracer-ics2018 <tracer@njuics.org>
2018-12-19 10:23:37 -0500

    >  compile U201514566 root Linux localhost.localdomain 3.10.0-862.14.4.el7.x86_64 #1 SMP Wed Sep 26 15:12:11 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  10:23:37 up 36 days,  5:53,  3 users,  load average: 0.07, 0.05, 0.06 156a6badf87cb4277b2c524eed5746fbe8c930d2

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -7,7 +7,6 @@ make_EHelper(lea);
 make_EHelper(call);
 make_EHelper(cltd);
 make_EHelper(leave);
-make_EHelper(test);
 make_EHelper(jcc);
 make_EHelper(operand_size);
 make_EHelper(push);
@@ -15,12 +14,16 @@ make_EHelper(pop);
 make_EHelper(ret);
 make_EHelper(inv);
 make_EHelper(nemu_trap);
+
 make_EHelper(sub);
 make_EHelper(add);
 make_EHelper(adc);
+make_EHelper(imul1);
 make_EHelper(imul2);
 make_EHelper(idiv);
 make_EHelper(cmp);
+
+make_EHelper(test);
 make_EHelper(xor);
 make_EHelper(and);
 make_EHelper(or);
