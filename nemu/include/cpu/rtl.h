3bcf1c5508ff9eed74f62b88167f69c9b3a238af
tracer-ics2018 <tracer@njuics.org>
2018-12-18 20:13:47 -0500

    >  compile U201514566 root Linux localhost.localdomain 3.10.0-862.14.4.el7.x86_64 #1 SMP Wed Sep 26 15:12:11 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  20:13:47 up 35 days, 15:43,  3 users,  load average: 0.01, 0.07, 0.10 38af23fd11a657c0fc23e1072a76412ffe3e1ab

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -185,7 +185,7 @@ static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
 
 #define make_rtl_setget_eflags(f) \
   static inline void concat(rtl_set_, f) (const rtlreg_t* src) { \
-    cpu.eflags.f = *src & 1;\
+    cpu.eflags.f = *src & 0x1;\
   } \
   static inline void concat(rtl_get_, f) (rtlreg_t* dest) { \
     *dest =  cpu.eflags.f;\
