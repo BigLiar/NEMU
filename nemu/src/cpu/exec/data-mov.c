3225600e8001fa7c25b00e8e53ea8bc40bcf5286
tracer-ics2018 <tracer@njuics.org>
2018-12-18 21:15:28 -0500

    >  compile U201514566 root Linux localhost.localdomain 3.10.0-862.14.4.el7.x86_64 #1 SMP Wed Sep 26 15:12:11 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  21:15:28 up 35 days, 16:45,  3 users,  load average: 0.07, 0.05, 0.05 f2ef7b3618247016775ff043d60ed4d584fb79a8

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -18,6 +18,7 @@ make_EHelper(push) {
 
 make_EHelper(pop) {
 	uint32_t cc = decoding.opcode & 0xf;
+	assert(0);
 	char reg_name[10] = "%";
 	strcat(reg_name, id_dest->width == 2 ? regsw[cc] : regsl[cc]);
 	strcpy(id_dest->str, reg_name);
