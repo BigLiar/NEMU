ec9012ca315044143e262c675b1dc7fb49c2c9ac
tracer-ics2018 <tracer@njuics.org>
2018-12-20 03:42:51 -0500

    >  compile U201514566 root Linux localhost.localdomain 3.10.0-862.14.4.el7.x86_64 #1 SMP Wed Sep 26 15:12:11 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  03:42:51 up 36 days, 23:12,  5 users,  load average: 0.08, 0.05, 0.07 acf5bbdcf0b78728c5e9a7ce33277790c8c46a1

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -56,7 +56,7 @@ make_EHelper(cmp) {
   rtl_and(&t2, &t2, &t1);
   rtl_msb(&t2, &t2, id_dest->width);
   rtl_set_OF(&t2);
-	printf("CF:%d, SF:%d, ZF:%d", cpu.eflags.CF, cpu.eflags.SF, cpu.eflags.ZF);
+	printf("CF:%d, SF:%d, ZF:%d\n", cpu.eflags.CF, cpu.eflags.SF, cpu.eflags.ZF);
 	print_asm_template2(cmp);
 }
 
