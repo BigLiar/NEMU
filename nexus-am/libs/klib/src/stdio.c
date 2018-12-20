79e9d6b1b4308674a3d9aac7ab77b27cd3c0d718
tracer-ics2018 <tracer@njuics.org>
2018-12-20 10:47:45 -0500

    >  run U201514566 root Linux localhost.localdomain 3.10.0-862.14.4.el7.x86_64 #1 SMP Wed Sep 26 15:12:11 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  10:47:45 up 37 days,  6:17,  3 users,  load average: 0.00, 0.01, 0.05 723ff74a5ca832577d360ce99071d7a0fed2df

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -3,6 +3,18 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+void sprintf_int(char *out, int* pos, int src){
+	if(src == 0)
+		return;
+	sprintf_int(out, pos, src / 10);
+	out[(*pos)++] = (char) src % 10 + '0';
+}
+
+void sprintf_str(char *out, int* pos, char* src){
+	*pos += strlen(src);
+	strcat(out, src);
+}
+
 int printf(const char *fmt, ...) {
   return 0;
 }
@@ -12,11 +24,36 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  return 0;
+  int i = 0;
+	int valint;
+	char* valstr;
+	va_list ap;
+	va_start(ap, fmt);
+	while(*fmt){
+		if(*fmt != '%')
+			out[i++] = *(fmt++);
+		else{
+			fmt++;
+			switch(*fmt){
+				case 'd':
+					valint = va_arg(ap, int);
+					if(valint == 0) out[i++] = '0';
+					else sprintf_int(out, &i, valint);
+					break;
+				case 's':
+					valstr = va_arg(ap, char*);
+					sprintf_str(out, &i, valstr);
+					break;
+			}
+			fmt++;
+		}
+	}
+	return i;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
   return 0;
 }
+
 
 #endif
