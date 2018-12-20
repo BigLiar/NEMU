4fa3fb2f19354bf301fbe324167191a7e15312fb
tracer-ics2018 <tracer@njuics.org>
2018-12-20 04:08:06 -0500

    >  compile U201514566 root Linux localhost.localdomain 3.10.0-862.14.4.el7.x86_64 #1 SMP Wed Sep 26 15:12:11 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  04:08:06 up 36 days, 23:37,  5 users,  load average: 0.27, 0.14, 0.08 9af8d039a09ce2514121b2b8c6b08ea3974c52fe

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -7,7 +7,6 @@ make_EHelper(test) {
 	rtl_set_CF(&t1);
 	rtl_set_OF(&t1);
 	rtl_update_ZFSF(&t0, id_dest->width);
-  printf("0x%x, 0x%x\n", id_src->val, id_dest->val);
 	print_asm_template2(test);
 }
 
@@ -23,7 +22,6 @@ make_EHelper(xor) {
   rtl_xor(&t0, &id_dest->val, &id_src->val);
 	rtl_update_ZFSF(&t0, id_dest->width);
 	operand_write(id_dest, &t0);
-  printf("0x%x, 0x%x, 0x%x\n", id_src->val, id_dest->val, id_dest->width);
   print_asm_template2(xor);
 }
 
@@ -31,7 +29,6 @@ make_EHelper(or) {
   rtl_or(&t0, &id_dest->val, &id_src->val);
 	rtl_update_ZFSF(&t0, id_dest->width);
 	operand_write(id_dest, &t0);
-  printf("0x%x, 0x%x, 0x%x\n", id_src->val, id_dest->val, id_dest->width);
  	print_asm_template2(or);
 }
 
