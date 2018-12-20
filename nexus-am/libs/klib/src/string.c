179f2f8c729a8f466d6d262adf4adde0baa97917
tracer-ics2018 <tracer@njuics.org>
2018-12-20 09:52:45 -0500

    >  run U201514566 root Linux localhost.localdomain 3.10.0-862.14.4.el7.x86_64 #1 SMP Wed Sep 26 15:12:11 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  09:52:45 up 37 days,  5:22,  3 users,  load average: 0.09, 0.06, 0.05 79b0ba82fe6046ab6d163308e90bb15be69af2d

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -3,39 +3,90 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  return 0;
+  size_t len = 0;
+	while(*(s++))
+		len++;
+	return len;
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+  size_t i;
+	for(i = 0; src[i] != '\0'; i++)
+			dst[i] = src[i];
+	dst[i] = '\0';
+	return dst;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+  size_t i;
+	for(i = 0; i < n && src[i] != '\0'; i++)
+			dst[i] = src[i];
+	for(; i < n; ++i)
+			dst[i] = '\0';
+	return dst;
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+  size_t len = strlen(dst);
+	size_t i;
+	for(i = 0; src[i] != '\0'; i++)
+			dst[len + i] = src[i];
+	dst[len + i] = '\0';
+	return dst;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+  size_t len1 = strlen(s1);
+	size_t len2 = strlen(s2);
+	size_t len = len1 > len2 ? len1 : len2;
+	size_t i;
+	for(i = 0; i < len; ++i){
+		if(s1[i] > s2[i]) return 1;
+		if(s1[i] < s2[i]) return -1;
+	}
+	return 0;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  return 0;
+  size_t len1 = strlen(s1);
+	size_t len2 = strlen(s2);
+	size_t len = len1 > len2 ? len1 : len2;
+	size_t i;
+	for(i = 0; i < n && i < len; ++i){
+		if(s1[i] > s2[i]) return 1;
+		if(s1[i] < s2[i]) return -1;
+	}
+	return 0;
 }
 
 void* memset(void* v,int c,size_t n) {
-  return NULL;
+  size_t i;
+	char* cv = (char*)v;
+	for(i = 0; i < n; ++i)
+		cv[i] = c && 0xff;
+	return v;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  return NULL;
+	assert(!(out < in && out + n > in));
+	assert(!(in < out && in + n > out));
+  size_t i;
+	char* cout = (char*)out;
+	char* cin = (char*)in;
+	for(i = 0; i < n; ++i)
+		cout[i] = cin[i];
+	return out;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
-  return 0;
+  unsigned char* ucs1 = (unsigned char*)s1;
+	unsigned char* ucs2 = (unsigned char*)s2;
+	size_t i;
+	for(i = 0; i < n; ++i){
+		if(ucs1[i] > ucs2[i]) return 1;
+		if(ucs1[i] < ucs2[i]) return -1;
+	}
+	return 0;
 }
 
 #endif
