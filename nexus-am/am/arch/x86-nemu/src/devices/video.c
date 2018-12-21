f634e7a5b45c662bd0630a85b73ed89c00fa8d10
tracer-ics2018 <tracer@njuics.org>
2018-12-21 13:35:04 -0500

    >  compile U201514566 root Linux localhost.localdomain 3.10.0-862.14.4.el7.x86_64 #1 SMP Wed Sep 26 15:12:11 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  13:35:04 up 38 days,  9:04,  3 users,  load average: 0.00, 0.02, 0.05 99b093acb8fb49d8818bee29b433ecde190925eb

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -2,15 +2,16 @@
 #include <x86.h>
 #include <amdev.h>
 #include <klib.h>
-
+#define W 400
+#define H 300
 static uint32_t* const fb __attribute__((used)) = (uint32_t *)0x40000;
 
 size_t video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
       _VideoInfoReg *info = (_VideoInfoReg *)buf;
-      info->width = 0;
-      info->height = 0;
+      info->width = W;
+      info->height = H;
       return sizeof(_VideoInfoReg);
     }
   }
@@ -22,7 +23,16 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
     case _DEVREG_VIDEO_FBCTL: {
       _FBCtlReg *ctl = (_FBCtlReg *)buf;
 
-      if (ctl->sync) {
+			int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+			int j;
+      uint32_t *pixels = ctl->pixels;
+      int cp_bytes = sizeof(uint32_t) * (w <  W - x ? w : W - x);
+      for (j = 0; j < h && y + j < H; j ++) {
+        memcpy(&fb[(y + j) * W + x], pixels, cp_bytes);
+        pixels += w;
+      }
+
+			if (ctl->sync) {
         // do nothing, hardware syncs.
       }
       return sizeof(_FBCtlReg);
