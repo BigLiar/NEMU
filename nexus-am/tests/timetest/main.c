751d4df23bfa6fa772671400622e9ccc258fef7f
tracer-ics2018 <tracer@njuics.org>
2018-12-20 22:50:49 -0500

    >  run U201514566 root Linux localhost.localdomain 3.10.0-862.14.4.el7.x86_64 #1 SMP Wed Sep 26 15:12:11 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  22:50:49 up 37 days, 18:20,  3 users,  load average: 0.06, 0.23, 0.23 6c6cd4ff710fff92895391ad2010892d20b93e5b

--- nexus-am/tests/timetest/main.c
+++ nexus-am/tests/timetest/main.c
@@ -9,8 +9,7 @@ int main(){
   while (1) {
     while(uptime() < 1000 * sec) ;
     get_timeofday(&rtc);
-		int a = 120;
-		printf("%d\n", a);
+		printf("%d\n", 123);
     printf("%d-%d-%d %02d:%02d:%02d GMT (", rtc.year, rtc.month, rtc.day, rtc.hour, rtc.minute, rtc.second);
     if (sec == 1) {
       printf("%d second).\n", sec);
