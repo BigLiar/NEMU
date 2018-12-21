b2b2d293c6d83b3f7b70a1c41c17f0356f8145f5
tracer-ics2018 <tracer@njuics.org>
2018-12-21 08:17:20 -0500

    >  run U201514566 root Linux localhost.localdomain 3.10.0-862.14.4.el7.x86_64 #1 SMP Wed Sep 26 15:12:11 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  08:17:20 up 38 days,  3:46,  6 users,  load average: 0.00, 0.01, 0.05 9b1914e2490d5b91add7daa97b64c4566eaed1b2

--- nexus-am/am/arch/x86-nemu/src/devices/timer.c
+++ nexus-am/am/arch/x86-nemu/src/devices/timer.c
@@ -1,12 +1,14 @@
 #include <am.h>
 #include <x86.h>
 #include <amdev.h>
+#include <stdio.h>
 #define TIME_PORT 0x48
 size_t timer_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_TIMER_UPTIME: {
       _UptimeReg *uptime = (_UptimeReg *)buf;	
 			uptime->lo = inl(TIME_PORT);
+			printf("uptime->lo:%d", uptime->lo);
       uptime->hi = 0;
       return sizeof(_UptimeReg);
     }
