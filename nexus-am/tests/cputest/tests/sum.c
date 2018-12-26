e11c96af03c0ef7384ed07f89cf55ef5f8530f4b
tracer-ics2018 <tracer@njuics.org>
2018-12-25 21:36:50 -0500

    >  run U201514566 root Linux localhost.localdomain 3.10.0-862.14.4.el7.x86_64 #1 SMP Wed Sep 26 15:12:11 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  21:36:50 up 42 days, 17:06,  4 users,  load average: 0.00, 0.02, 0.10 d79266fb78d4b9d6c2a507cdd3f60b71220d5ac5

--- nexus-am/tests/cputest/tests/sum.c
+++ nexus-am/tests/cputest/tests/sum.c
@@ -1,13 +1,9 @@
 #include "trap.h"
-
+#include <stdio.h>
 int main() {
-	int i = 1;
-	volatile int sum = 0;
-	for(i = 1; i <= 100; i++) {
-		sum += i;
-	}
-
-	nemu_assert(sum == 5050);
-
+	int i = 0x182741;
+	if(i == 0x182741)
+		printf("no error!");
+	printf("ox%x", i);
 	return 0;
 }
