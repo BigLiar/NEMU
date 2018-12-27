ae82c7d1d6238ba92b41c44a7c349a057438d00e
tracer-ics2018 <tracer@njuics.org>
2018-12-26 21:20:28 -0500

    >  compile U201514566 root Linux localhost.localdomain 3.10.0-862.14.4.el7.x86_64 #1 SMP Wed Sep 26 15:12:11 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  21:20:28 up 43 days, 16:50,  3 users,  load average: 0.08, 0.04, 0.05 521850cb51c6590ccff0fa0a2b718789eaa941e2

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -281,7 +281,7 @@ make_DHelper(cl_G2E) {
 
 make_DHelper(Ms){
   decode_op_rm(eip, id_src, false, NULL, false);
-	printf("0x%8x\n, 0x%8x\n", id_dest->addr, id_src->addr);
+	printf("0x%8x\n", id_dest->addr);
 	rtl_lm(&id_src->val, &id_dest->addr, 2);
 	rtl_lr(&t0, 2, 4);
 	rtl_add(&t0, &id_dest->addr, &t0);
