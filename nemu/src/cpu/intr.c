4f68ddb8e9b54ac9689064eda10ac3c8f24ae80b
tracer-ics2018 <tracer@njuics.org>
2018-12-26 22:22:03 -0500

    >  compile U201514566 root Linux localhost.localdomain 3.10.0-862.14.4.el7.x86_64 #1 SMP Wed Sep 26 15:12:11 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  22:22:03 up 43 days, 17:51,  3 users,  load average: 0.02, 0.04, 0.05 f96d59dbcc3868725b31305191dd5cdd5d9a057

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -5,7 +5,7 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
-
+	assert(0);
   rtl_pop((uint32_t *)&cpu.eflags);
 	rtl_pop(&cpu.cs);
 	rtl_pop(&ret_addr);
