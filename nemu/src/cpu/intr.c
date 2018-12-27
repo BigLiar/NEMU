b6141aa1825071570217fa9bfede2596cd44bd1c
tracer-ics2018 <tracer@njuics.org>
2018-12-26 22:24:24 -0500

    >  compile U201514566 root Linux localhost.localdomain 3.10.0-862.14.4.el7.x86_64 #1 SMP Wed Sep 26 15:12:11 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  22:24:24 up 43 days, 17:53,  3 users,  load average: 0.02, 0.04, 0.05 e27513e7b653443afc3ad021b37815e77d6536d

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -8,11 +8,10 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   rtl_pop((uint32_t *)&cpu.eflags);
 	rtl_pop(&cpu.cs);
 	rtl_pop(&ret_addr);	
-	rtl_lr(&t0, cpu.IDTR.len, 2);
+	t0 = cpu.IDTR.len & 0xffff;
 	t1 = NO * 8;
-	assert(0);
 	assert(interpret_relop(RELOP_LT, t1, t0));	
-	rtl_lr(&t0, cpu.IDTR.base, 4);
+	t0 = cpu.IDTR.base;
 	rtl_add(&t0, &t1, &t0);
 	printf("0x%08x\n", t0);
 	rtl_lm(&t1, &t0, 2);
