7c3659e19ed1c81e1e78e295c4d34c7fa757a7e1
tracer-ics2018 <tracer@njuics.org>
2018-12-28 00:09:52 -0500

    >  run U201514566 root Linux localhost.localdomain 3.10.0-862.14.4.el7.x86_64 #1 SMP Wed Sep 26 15:12:11 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  00:09:52 up 44 days, 19:39,  4 users,  load average: 0.06, 0.03, 0.05 29d86107c8f2e93f4fcb69edfacad571f99b024e

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,18 +1,28 @@
 #include "common.h"
 #include "syscall.h"
 
+
+int sys_write(){	
+	return 0;
+}
+
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+	a[1] = c->GPR2;
+	a[2] = c->GPR3;
+	a[3] = c->GPR4;
   switch (a[0]) {
-		case 0x1: _yield();
+		case SYS_yield: _yield();
 							c->GPRx = 0;
 							break;
-		case 0x0: _halt(0);
+		case SYS_exit: _halt(0);
 							c->GPRx = 0;
 							break;
+		case SYS_write: sys_write();
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
   return NULL;
 }
+
