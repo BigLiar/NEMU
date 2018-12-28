0c32c4a62b0c4079e292ad039d7d6e9bd74ffc3e
tracer-ics2018 <tracer@njuics.org>
2018-12-28 01:03:14 -0500

    >  run U201514566 root Linux localhost.localdomain 3.10.0-862.14.4.el7.x86_64 #1 SMP Wed Sep 26 15:12:11 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  01:03:14 up 44 days, 20:32,  4 users,  load average: 0.04, 0.10, 0.08 aea63f7657341d1ba5ffe4a7f186182ed34e0b60

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -9,8 +9,7 @@
 intptr_t _syscall_(int type, intptr_t a0, intptr_t a1, intptr_t a2){
   int ret = -1;
   asm volatile("int $0x80": "=a"(ret): "a"(type), "b"(a0), "c"(a1), "d"(a2));
-	if(ret == 0)
-			assert(0);
+
 	return ret;
 }
 #elif defined(__ISA_AM_NATIVE__)
