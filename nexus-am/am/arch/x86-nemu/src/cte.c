a448aeac7b7372625ced55df461b9d28811f85bc
tracer-ics2018 <tracer@njuics.org>
2018-12-27 22:27:23 -0500

    >  run U201514566 root Linux localhost.localdomain 3.10.0-862.14.4.el7.x86_64 #1 SMP Wed Sep 26 15:12:11 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  22:27:23 up 44 days, 17:56,  3 users,  load average: 0.14, 0.13, 0.09 49e5c201aeec8eade4ee1c9343e59f4292e9ea8d

--- nexus-am/am/arch/x86-nemu/src/cte.c
+++ nexus-am/am/arch/x86-nemu/src/cte.c
@@ -10,6 +10,7 @@ _Context* irq_handle(_Context *tf) {
   _Context *next = tf;
   if (user_handler) {
     _Event ev = {0};
+		printf("irq:0x%d\n", tf->irq);
     switch (tf->irq) {
 			case 0x81: ev.event = _EVENT_YIELD; break;
 			case 0x80: ev.event = _EVENT_SYSCALL; break;
