76fbdc3078cc9d44f7e14705b911deba9d5933a1
tracer-ics2018 <tracer@njuics.org>
2018-12-27 21:42:20 -0500

    >  run U201514566 root Linux localhost.localdomain 3.10.0-862.14.4.el7.x86_64 #1 SMP Wed Sep 26 15:12:11 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  21:42:20 up 44 days, 17:11,  3 users,  load average: 0.00, 0.05, 0.14 9faef2d6f0d62f7e9c003a4af184081e8130637e

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,7 +2,8 @@
 
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
-		case _EVENT_YIELD: panic("This is event yield and ID = %d", e.event);
+		case _EVENT_YIELD: printf("This is event yield and ID = %d", e.event);
+											 break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
