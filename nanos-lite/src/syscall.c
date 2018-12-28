6ab01b4ccb87fe5d260b0c4027fa2baa827ccf09
tracer-ics2018 <tracer@njuics.org>
2018-12-28 00:46:48 -0500

    >  run U201514566 root Linux localhost.localdomain 3.10.0-862.14.4.el7.x86_64 #1 SMP Wed Sep 26 15:12:11 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  00:46:48 up 44 days, 20:16,  4 users,  load average: 0.18, 0.10, 0.06 104d824897b35419fe7d7263c292fdf5e61f969

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -8,8 +8,8 @@ int sys_write(int fd, void* buf, size_t count){
 		size_t i;
 		char *cbuf = (char *)buf;
 		for(i = 0; i < count && cbuf[i] != 0; i++)
-		_putc(cbuf[i]);
-		return i;
+			_putc(cbuf[i]);
+		return 2;
 	}
 	return -1;
 }
