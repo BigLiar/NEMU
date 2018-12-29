3b17101d3194ebbaba3df841f51c242c2a7676fe
tracer-ics2018 <tracer@njuics.org>
2018-12-29 08:32:49 -0500

    >  run U201514566 root Linux localhost.localdomain 3.10.0-862.14.4.el7.x86_64 #1 SMP Wed Sep 26 15:12:11 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  08:32:49 up 46 days,  4:02,  4 users,  load average: 0.08, 0.03, 0.10 a8a5c5c9a5f9c2294e18d3074789488828634ea

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -99,6 +99,7 @@ off_t sys_lseek(int fd, off_t offset, int whence) {
 }
 
 size_t fs_write(int fd, void* buf, size_t count){
+	Log("fs_write:fd:%d\n", fd);
 	assert(NR_FILES > fd);
 	if(file_table[fd].write == NULL)
 		return sys_write(fd, buf, count);
