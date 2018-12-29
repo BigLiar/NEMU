16f3eec4c9a9bd937a6fff29c5167007fb0b2791
tracer-ics2018 <tracer@njuics.org>
2018-12-29 10:03:30 -0500

    >  run U201514566 root Linux localhost.localdomain 3.10.0-862.14.4.el7.x86_64 #1 SMP Wed Sep 26 15:12:11 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  10:03:30 up 46 days,  5:33,  4 users,  load average: 0.38, 0.12, 0.12 25d4c4428dffe0beeee70ac9cf1963b7ae24793

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -40,6 +40,7 @@ int _write(int fd, void *buf, size_t count){
 void *_sbrk(intptr_t increment){
 	intptr_t addr = 0;
 	_syscall_(SYS_brk, (intptr_t)&addr,  increment, 0);
+	printf("addr:0x%08x\n", addr);
 	return (void *)addr;
 }
 
