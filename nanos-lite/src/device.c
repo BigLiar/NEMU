e6d94d06f1ba26a9692e50de0b89670578cca7b9
tracer-ics2018 <tracer@njuics.org>
2018-12-29 08:22:57 -0500

    >  run U201514566 root Linux localhost.localdomain 3.10.0-862.14.4.el7.x86_64 #1 SMP Wed Sep 26 15:12:11 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  08:22:57 up 46 days,  3:52,  4 users,  load average: 0.02, 0.08, 0.17 45e708b777641fc97d272866714c8dea55d7e984

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -1,12 +1,26 @@
 #include "common.h"
 #include <amdev.h>
 
+static int W, H;
+
 size_t serial_write(const void *buf, size_t offset, size_t len) {
 	size_t i;
 	char *cbuf = (char *)buf;
 	for(i = 0; i < len && cbuf[i] != 0; i++)
 		_putc(cbuf[i]);
 	return i;
+}
+
+static _Device *getdev(uint32_t id) {
+  int n;
+	for (n = 1; ; n ++) {
+    _Device *cur = _device(n);
+    if (cur->id == id)
+      return cur;
+    if (!cur) break;
+  }
+  assert(0);
+  return NULL;
 }
 
 #define NAME(key) \
@@ -29,21 +43,29 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   return len;
 }
 
+
+
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  return 0;
+	_Device *dev = getdev(_DEV_VIDEO);
+	_FBCtlReg ctl;
+  ctl.pixels = (uint32_t *)buf;
+ 	ctl.x = offset % W;
+ 	ctl.y	= offset / W;
+	ctl.w = 1;
+	ctl.h = 1;
+	int i;
+	for(i = 0; i < len; ++i){
+ 		if(ctl.y == H - 1 && ctl.x == W - 1)
+			return i; 	
+		dev->write(_DEVREG_VIDEO_FBCTL, &ctl, sizeof(ctl));
+		ctl.x++;
+		ctl.y += ctl.x / W;
+	 	ctl.x %= W;	
+		ctl.pixels++;
+	}		
+	return i;
 }
-static _Device *getdev(uint32_t id) {
-  int n;
-	for (n = 1; ; n ++) {
-    _Device *cur = _device(n);
-    if (cur->id == id)
-      return cur;
-    if (!cur) break;
-  }
-  assert(0);
-  return NULL;
-}
-int W, H;
+
 void init_device() {
   Log("Initializing devices...");
   _ioe_init();
