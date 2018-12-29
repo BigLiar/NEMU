3b40abfc51aeac20db690172df665d51cd16dfa3
tracer-ics2018 <tracer@njuics.org>
2018-12-29 08:31:13 -0500

    >  run U201514566 root Linux localhost.localdomain 3.10.0-862.14.4.el7.x86_64 #1 SMP Wed Sep 26 15:12:11 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  08:31:13 up 46 days,  4:00,  4 users,  load average: 0.00, 0.01, 0.11 1f4d2e67d6d6d27950a600aeaf34294847bed569

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -46,6 +46,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
+	Log("fb_write: offset:0x%08x, len:%d\n", offset, len);
 	_Device *dev = getdev(_DEV_VIDEO);
 	_FBCtlReg ctl;
   ctl.pixels = (uint32_t *)buf;
